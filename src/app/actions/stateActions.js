258f17706b5feefcd66eb999b21b627cf85d14ca
Richard <lapislacrima@gmail.com>
2016-12-16 13:33:39 -0800

    fetch replaced with $.ajax
    Now uses JSONP from $.ajax to fetch from the wikipedia api parsed html of the wikipedia page

--- src/app/actions/stateActions.js
+++ src/app/actions/stateActions.js
@@ -1,45 +1,66 @@
 import {MAX_HINT, MAX_QUESTION} from '../main.js';
-import parseWikitext from 'txtwiki';
-
-const fetchOptions = {
-  method: 'GET',
-  mode: 'no-cors'
-};
+import update from 'immutability-helper';
 
 export function addHint() {
-  let output: {};
+  console.log('Adding a new Hint');
+  console.log(this.state.data);
   if (this.state.data.hintCount >= MAX_HINT) {
     console.warn('Number of hints already at maximum');
-  } else {
-    output = {hintCount: this.state.data.hintCount + 1};
+    return;
   }
-  return output;
+  this.setState({
+    data: update(this.state.data, {hintCount: {$set: this.state.data.hintCount + 1}})
+  });
 }
 
 export function newQuestion(difficulty = 'easy') {
+  console.log('fetching new questions');
   if (this.state.data.questionCount >= MAX_QUESTION) {
     return;
   }
-  function wikiTextFetch(cityName) {
-    fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${cityName}&prop=revisions&rvprop=content&format=json`, fetchOptions);
-  }
-  function randomCity(difficulty) {
-    console.log(difficulty);
-    return "Uruk";
-  }
+
   return randomCity(difficulty)
     .then(cityName => {
       return wikiTextFetch(cityName);
     })
-    .then(outputString => {
-      console.log('??????????????????');
-      console.log('outputString');
-      parseWikitext(outputString);
+    .then(responseObj => {
+      console.log(responseObj.query);
+      console.log(typeof (responseObj));
+      return responseObj();
     })
     .then(parsedString => {
       const parsedQuestions = parsedString.split('.').slice(0, 4);
-      console.log('!!!!!!!!!!!!!!!!!!!!!!!!');
-      console.log(parsedQuestions);
-      this.setState({data: {questionList: parsedQuestions}});
+      if (parsedQuestions) {
+        this.setState({data: {questionList: parsedQuestions}});
+      }
     });
 }
+
+function wikiTextFetch(cityName) {
+  return (
+    window.$.ajax({
+      url: 'https://en.wikipedia.org/w/api.php',
+      jsonp: 'callback',
+      dataType: 'jsonp',
+      data: {
+        action: 'query',
+        titles: cityName,
+        prop: 'extracts',
+        format: 'json'
+      }
+    })
+  );
+}
+
+function randomCity(difficulty) {
+  // eslint-disable-next-line
+  return new Promise((resolve, reject) => {
+    console.log(difficulty);
+    console.log('placeholder city Uruk');
+    resolve('Uruk');
+  });
+}
+
+// function parseResponse(responseObj){
+//   const query = responseObj.query.pages
+// }
