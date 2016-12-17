9369e96ab305d3fccf91a6f6805074723082aba8
Richard <lapislacrima@gmail.com>
2016-12-16 22:28:49 -0800

    fetch location with Google Map Api; multiple setState
    Google Map Api's geocoder is called as a promise to find a city's coordinates
    the setState logic has been changed to alter multiple state.data values simultaneously

--- src/app/actions/stateActions.js
+++ src/app/actions/stateActions.js
@@ -1,6 +1,8 @@
 import {MAX_HINT, MAX_QUESTION} from '../main.js';
 import update from 'immutability-helper';
 import parseWikiResponse from './parser.js';
+// import parseWikiLocation from './parseLocation.js';
+const Promise = require('bluebird');
 
 export function addHint() {
   console.log('Adding a new Hint');
@@ -16,12 +18,17 @@ export function addHint() {
 
 export function newQuestion(difficulty = 'easy') {
   console.log('fetching new questions');
+  let answerLocation;
   if (this.state.data.questionCount >= MAX_QUESTION) {
     return;
   }
 
   return randomCity(difficulty)
     .then(cityName => {
+      googleLocation(cityName)
+        .then(locationResponseObj => {
+          answerLocation = locationResponseObj;
+        });
       return wikiTextFetch(cityName);
     })
     .then(responseObj => {
@@ -34,8 +41,12 @@ export function newQuestion(difficulty = 'easy') {
         this.setState({
           data: update(
             this.state.data,
-            {questionList: {$set: parsedQuestions}},
-            {hintCount: {$set: 0}})
+            {
+              questionList: {$set: parsedQuestions},
+              hintCount: {$set: 0},
+              answer: {$set: answerLocation}
+            }
+            )
         });
       }
     });
@@ -57,11 +68,24 @@ function wikiTextFetch(cityName) {
   );
 }
 
+function googleLocation(cityName) {
+  return new Promise((resolve, reject) => {
+    const geocoder = new window.google.maps.Geocoder();
+    geocoder.geocode({address: cityName}, (results, status) => {
+      if (status === window.google.maps.GeocoderStatus.OK) {
+        resolve([results[0].geometry.location.lat(), results[0].geometry.location.lng()]);
+      } else {
+        reject(status);
+      }
+    });
+  });
+}
+
 function randomCity(difficulty) {
   // eslint-disable-next-line
   return new Promise((resolve, reject) => {
     console.log(difficulty);
-    resolve('Uruk');
+    resolve('Chang\'an');
   });
 }
 
