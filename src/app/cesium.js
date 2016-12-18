50feb8c2bc247305a03b6897a1668438d250c280
Richard <lapislacrima@gmail.com>
2016-12-17 16:51:32 -0800

    define the Alkali element
    the alkali element contains the three divs defined in the Cesium Hello World example

---
+++ src/app/cesium.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Alkali extends React.Component {
+
+  render() {
+    // eslint-disable-next-line
+    return ( // eslint-disable-next-line
+      <div>
+        <div id="cesiumContainer"></div>
+        <div id="loadingOverlay"><h1>Loading...</h1></div>
+        <div id="toolbar"></div>
+      </div>
+    );
+  }
+}
+
