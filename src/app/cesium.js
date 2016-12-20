95a849edfd3e880a89758b458d74225ab5f574b4
Graham Lee <wgin.lee@gmail.com>
2016-12-19 21:53:00 -0800

    got canvas to show with react

--- src/app/cesium.js
+++ src/app/cesium.js
@@ -1,6 +1,15 @@
 import React from 'react';
+// import CesiumWidget from 'cesium/Source/Widgets/CesiumWidget/CesiumWidget';
+import BuildModuleUrl from 'cesium/Source/Core/buildModuleUrl';
+BuildModuleUrl.setBaseUrl('./');
+import CesiumViewer from 'cesium/Source/Widgets/Viewer/Viewer';
 
 export default class Alkali extends React.Component {
+
+  componentDidMount() {
+    // Create the Cesium Viewer
+    this.viewer = new CesiumViewer('cesiumContainer');
+  }
 
   render() {
     // eslint-disable-next-line
