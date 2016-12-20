3347daeaf38bedd0a1b96ecb691da62bd1f34d55
Graham Lee <wgin.lee@gmail.com>
2016-12-20 11:14:22 -0800

    made globe show up in react

--- src/app/cesium.js
+++ src/app/cesium.js
@@ -2,13 +2,13 @@ import React from 'react';
 // import CesiumWidget from 'cesium/Source/Widgets/CesiumWidget/CesiumWidget';
 import BuildModuleUrl from 'cesium/Source/Core/buildModuleUrl';
 BuildModuleUrl.setBaseUrl('./');
-import CesiumViewer from 'cesium/Source/Widgets/Viewer/Viewer';
+// import CesiumViewer from 'cesium/Source/Widgets/Viewer/Viewer';
 
 export default class Alkali extends React.Component {
 
   componentDidMount() {
     // Create the Cesium Viewer
-    this.viewer = new CesiumViewer('cesiumContainer');
+    this.viewer = new window.Cesium.Viewer('cesiumContainer');
   }
 
   render() {
