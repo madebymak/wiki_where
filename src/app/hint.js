c51f66d5f0eb067708f9dc608e2e1316a8e0c7be
madebymak <mr.jmak@gmail.com>
2016-12-30 13:54:20 -0800

    Changed hint/next button to material-ui style.

--- src/app/hint.js
+++ src/app/hint.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import {deepOrange500} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500
+  }
+});
 
 export default class Hint extends React.Component {
   constructor(props) {
     super(props);
-    this.submitHandler = this.submitHandler.bind(this);
+    this.handleSubmitHandler = this.handleSubmitHandler.bind(this);
   }
-  submitHandler(e) {
+  handleSubmitHandler(e) {
     e.preventDefault();
     this.props.addHint();
   }
 
   render() {
     return ( // eslint-disable-next-line
-      <button type="button" ref={el => this.newHint = el} onClick={this.submitHandler}>
-        Hint
-      </button>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <FlatButton className="hint-next-btn" label="hint" key="1" onTouchTap={this.handleSubmitHandler}/>
+        {/* // <button type="button" ref={el => this.newHint = el} onClick={this.submitHandler}>
+        //   Hint
+        // </button> */}
+      </MuiThemeProvider>
     );
   }
 }
--- src/app/renew.js
+++ src/app/renew.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import {deepOrange500} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500
+  }
+});
 
 export default class Renew extends React.Component {
   constructor(props) {
     super(props);
-    this.submitHandler = this.submitHandler.bind(this);
+    this.handleSubmitHandler = this.handleSubmitHandler.bind(this);
   }
-  submitHandler(e) {
+  handleSubmitHandler(e) {
     e.preventDefault();
     this.props.newQuestion();
   }
 
   render() {
     return ( // eslint-disable-next-line
-      <button type="button" ref={el => this.newHint = el} onClick={this.submitHandler}>
-        Next Question
-      </button>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <FlatButton className="hint-next-btn" label="Next" key="1" onTouchTap={this.handleSubmitHandler}/>
+        {/* <button type="button" ref={el => this.newHint = el} onClick={this.submitHandler}>
+          Next Question
+        </button>   */}
+      </MuiThemeProvider>
+
     );
   }
 }
