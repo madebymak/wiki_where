957ce997b2be7817acd16e72458549fe424ef83f
Graham Lee <wgin.lee@gmail.com>
2017-01-10 21:18:36 -0800

    view doesn't default on america anymore but instead zooms out from the previous answer line

--- src/app/cesium.js
+++ src/app/cesium.js
@@ -118,7 +118,9 @@ export default class Alkali extends React.Component {
       answer.show = false;
       line.show = false;
       if (this.props.flyHomeSwitch === true) {
-        this.viewer.camera.flyHome();
+        this.viewer.camera.flyTo({
+          destination: new Cesium.Cartesian3(this.viewer.camera.position.x, this.viewer.camera.position.y, this.viewer.scene.screenSpaceCameraController.maximumZoomDistance)
+        });
         const guess = this.viewer.entities.getById('guess');
         guess.show = false;
         this.props.flewHome();
